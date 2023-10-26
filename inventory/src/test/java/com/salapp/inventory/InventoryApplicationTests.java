8e967c6f321c9f704baabb1944b1fb36a66a0493
Stainley Lebron <stainley.lebron@gmail.com>
2023-10-25 21:30:35 -0400

    inventory

---
+++ inventory/src/test/java/com/salapp/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salapp.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
